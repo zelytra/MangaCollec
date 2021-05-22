f3ea9f89a01e11563c51cda807ac2893d56d5faf
Zelytra <alexbreuillet@gmail.com>
2021-05-22 19:23:38 +0200

    Add cache storage

--- app/src/main/java/fr/zelytra/mangacollec/MainActivity.java
+++ app/src/main/java/fr/zelytra/mangacollec/MainActivity.java
@@ -1,5 +1,7 @@
 package fr.zelytra.mangacollec;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import fr.zelytra.mangacollec.api.Movie;
 import fr.zelytra.mangacollec.api.MovieApi;
 import fr.zelytra.mangacollec.api.RestMovieResponse;
@@ -26,14 +29,33 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter listAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("app", "making api call");
-        apiCall();
+        sharedPreferences = getSharedPreferences("moviesList", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        List<Movie> movieList = getDataFromLocal();
+        if(movieList!=null)
+            showList(movieList);
+        else
+            apiCall();
+    }
+
+    private List<Movie> getDataFromLocal() {
+        String jsonMovies = sharedPreferences.getString("jsonMovie",null);
+        if(jsonMovies==null)
+            return null;
+        else
+            return gson.fromJson(jsonMovies,new TypeToken<List<Movie>>(){}.getType());
+
     }
 
     private void showList(List<Movie> movieList) {
@@ -53,9 +75,6 @@ public class MainActivity extends AppCompatActivity {
     private void apiCall() {
         final String BASE_URL = "https://api.themoviedb.org/3/";
         final String apiKey = "3e2de8ad359733ffda6986280aa735dd";
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -70,18 +89,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RestMovieResponse> call, Response<RestMovieResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    showList(response.body().getResults());
+                    List<Movie> movieList = response.body().getResults();
                     Toast.makeText(MainActivity.this, "API succed", Toast.LENGTH_SHORT).show();
-
-                }else {
+                    saveList(movieList);
+                } else {
                     Toast.makeText(MainActivity.this, "API failed", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<RestMovieResponse> call, Throwable t) {
-                Log.e("app","error");
+                Log.e("app", "error");
             }
         });
+    }
+
+    private void saveList(List<Movie> movieList) {
+        String jsonString = gson.toJson(movieList);
+        sharedPreferences
+                .edit()
+                .putString("jsonMovie",jsonString)
+                .apply();
+
     }
 }
